d5c4a707a5c1c061848787439b3a42364c873d93
s.kazantsev <s.kazantsev@skyeng,.ru>
2021-05-19 18:55:53 +0300

    hw3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const catalogData = '/catalogData.json';
+const getBasket = '/getBasket.json';
+const addToBasket = '/addToBasket.json';
+const deleteFromBasket = '/deleteFromBasket.json';
+
+function makeRequest(url, action, callback) {
+    let xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+
 class BaseItem {
     constructor(title, price) {
         this.title = title;
@@ -33,22 +56,29 @@ class ProductItem extends BaseItem {
 
 class CartList {
     constructor() {
-        this.goods = [];
+        this.goods = {};
     }
 
-    fetchGoods() {
-        this.goods = [
-            {title: 'Shirt', price: 150},
-            {title: 'Socks', price: 50},
-            {title: 'Jacket', price: 350},
-            {title: 'Shoes', price: 250},
-        ];
+    fetchGoods(callback) {
+        // this.goods = [
+        //     {title: 'Shirt', price: 150},
+        //     {title: 'Socks', price: 50},
+        //     {title: 'Jacket', price: 350},
+        //     {title: 'Shoes', price: 250},
+        // ];
+        makeRequest(API + getBasket, 'GET',
+            response => {
+                this.goods = JSON.parse(response)
+                callback();
+            }
+        )
     }
 
     render() {
         let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new CartItem(good.title, good.price);
+        console.log(this);
+        this.goods.contents.forEach(good => {
+            const goodItem = new CartItem(good.product_name, good.price);
             listHtml += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
@@ -61,17 +91,25 @@ class CartList {
 
 class ProductList {
     constructor() {
-        this.products = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+        // this.products = [
+        //     {id: 1, title: 'Notebook', price: 2000},
+        //     {id: 2, title: 'Mouse', price: 20},
+        //     {id: 3, title: 'Keyboard', price: 200},
+        //     {id: 4, title: 'Gamepad', price: 50},
+        // ];
+    }
+
+    getProducts(callback) {
+        makeRequest(API + catalogData, 'GET',
+            (request => {
+                this.products = JSON.parse(request)
+                callback()
+            }))
     }
 
     render() {
         const productsList = this.products
-            .map(item => new ProductItem(item.title, item.price, item.img)
+            .map(item => new ProductItem(item.product_name, item.price, item.img)
                 .render());
         document.querySelector('.products').innerHTML = productsList.join('');
     }
@@ -80,11 +118,10 @@ class ProductList {
 
 const renderPage = () => {
     let cart = new CartList();
-    cart.fetchGoods();
-    cart.render();
+    cart.fetchGoods(() => cart.render());
 
     let products = new ProductList();
-    products.render();
+    products.getProducts(() => products.render());
 };
 
 renderPage();
